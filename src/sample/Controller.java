b1bd81b66a39f667fe3c1743c7141b3d92836821
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-08-25 19:05:28 +0530

    Added a basic button

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+    @FXML
+    private void printHelloWorld(ActionEvent event) {
+        event.consume();
+        System.out.println("Hello World");
+    }
 }
